b5192b9d1e91b5e0ae8d88e9acd3815406a15602
Eva Doncieux <eva.doncieux@gmail.com>
2025-09-02 17:15:45 +0200

    feature | adding member sign up tests

--- backend/application/use-cases/AuthenticateUseCase.ts
+++ backend/application/use-cases/AuthenticateUseCase.ts
@@ -1,20 +1,20 @@
-import {Member} from "@domain/entities/Member";
-import {MemberRepository} from "@domain/interfaces/MemberRepository";
+import {Gender, Member, MembershipType} from "../../domain/entities/Member";
+import {InMemoryMemberRepository} from "../../infrastructure/repositories/InMemoryMemberRepository";
 import {BasicAuthenticator} from "../../infrastructure/auth/basic-authenticator";
 
 type PayloadUseCase = {
     password: string,
-    email: string
+    email: string,
+    gender: Gender;
+    firstname: string;
+    lastname: string;
 }
 
 export class AuthenticateUseCase {
-    constructor(private memberRepository: MemberRepository) {
+    constructor(private memberRepository: InMemoryMemberRepository) {
     }
 
-    async execute(payload: {
-        password: any,
-        email: any,
-    }): Promise<Member | null> {
+    async authenticateMember(payload: PayloadUseCase): Promise<Member | null> {
         const member = await this.memberRepository.findByEmail(payload.email);
         let authenticator = new BasicAuthenticator(this.memberRepository);
 
@@ -22,11 +22,63 @@ export class AuthenticateUseCase {
             throw new Error('Could not find member with email: ' + payload.email);
         }
 
-        member.validOrThrow();
+        member.validOrThrow("authentication");
 
         if (!await authenticator.authenticate(payload.email, payload.password)) {
             throw new Error('Invalid credentials');
         }
         return member;
+    }
+
+    async addMember(payload: {
+        gender: Gender;
+        firstname: any,
+        lastname: any,
+        password: any,
+        email: any,
+        bio: any,
+        birthdate: any,
+        city: any,
+        company: any,
+        country: any,
+        isManager: boolean,
+        joinDate: any,
+        membershipType: MembershipType,
+        phone: any,
+        photo: any,
+        profession: any,
+        skills: any,
+        id: any
+    }) {
+        const newMember = new Member({
+            gender: payload.gender,
+            firstname: payload.firstname,
+            bio: payload.bio,
+            birthdate: payload.birthdate,
+            city: payload.city,
+            company: payload.company,
+            country: payload.country,
+            email: payload.email,
+            id: payload.id,
+            isManager: payload.isManager,
+            joinDate: payload.joinDate,
+            lastname: payload.lastname,
+            membershipType: payload.membershipType,
+            password: payload.password,
+            phone: payload.phone,
+            photo: payload.photo,
+            profession: payload.profession,
+            skills: payload.skills
+        })
+
+        newMember.validOrThrow("creation");
+        const members = await this.memberRepository.findAll();
+
+        if(newMember.hasConflictWith(members)) {
+            throw new Error('A member with the same email already exists');
+        }
+
+        await this.memberRepository.save(newMember);
+        return newMember.id;
     }
 }
--- backend/tests/authentication/authentication.test.ts
+++ backend/tests/authentication/authentication.test.ts
@@ -2,10 +2,10 @@ import {AuthenticateUseCase} from "../../application/use-cases/AuthenticateUseCa
 import {Member} from "../../domain/entities/Member";
 import {InMemoryMemberRepository} from "../../infrastructure/repositories/InMemoryMemberRepository";
 
-describe("Use case: member authentication", () => {
+describe("Use case: member management", () => {
     let memberRepository = new InMemoryMemberRepository();
     let useCase = new AuthenticateUseCase(memberRepository)
-    const testMember = new Member({
+    const validMember = new Member({
         id: "1",
         gender: 'female',
         firstname: 'Jane',
@@ -21,12 +21,36 @@ describe("Use case: member authentication", () => {
         membershipType: "Basic",
         password: "jane.doe",
         phone: "",
-        photo: "",
+        photo: "06-44-66-74-77",
         profession: "",
         skills: [],
     })
 
     it('should not authenticate a unknown member', async () => {
-        expect(useCase.execute(testMember))
+        expect(useCase.authenticateMember(validMember))
+    })
+
+    it('should not add a member with an existing email', async () => {
+        expect(useCase.addMember({
+            ...validMember,
+            firstname: 'Clara',
+            lastname: 'Lemoine',
+            email: "clara.lemoine@example.com",
+        }))
+    })
+
+    it('should not add a member with incomplete information', async () => {
+        expect(useCase.addMember({
+            ...validMember,
+            firstname: 'Clara',
+            lastname: null,
+            email: "clara.lemoine@example.com",
+        }))
+    })
+
+    it('should add a member', async () => {
+        expect(useCase.addMember({
+            ...validMember,
+        }))
     })
 });
