2aaae400284fa8beaae36d4b3e3a118b137c3ba2
Eva Doncieux <eva.doncieux@gmail.com>
2025-09-04 09:47:48 +0200

    feature | add booking handling use cases, service enhancements, booking repository, and tests

--- backend/domain/entities/Booking.ts
+++ backend/domain/entities/Booking.ts
@@ -4,6 +4,7 @@
  */
 
 export type BookingType = 'meetingRoom_booking' | 'privateRoom_booking' | 'event_registration';
+export type BookingStatus = 'pending' | 'confirmed' | 'declined';
 
 export interface BookingData {
     id: string;
@@ -12,15 +13,25 @@ export interface BookingData {
     endDate: string;
     type: BookingType;
     roomId: string;
+    status: BookingStatus
 }
+
+const REQUIRED_FIELDS: (keyof BookingData)[] = [
+    "memberId",
+    "startDate",
+    "endDate",
+    "type",
+    "roomId",
+]
 
 export class Booking {
     public readonly id: string;
-    public readonly memberId: string;
+    public memberId: string;
     public startDate: string;
     public endDate: string;
     public type: BookingType;
     public readonly roomId: string;
+    public status: BookingStatus;
 
     constructor(data: BookingData) {
         this.id = data.id;
@@ -29,5 +40,47 @@ export class Booking {
         this.endDate = data.endDate;
         this.type = data.type;
         this.roomId = data.roomId;
+        this.status = data.status;
+    }
+
+    validOrThrow() {
+        if(this.hasEmptyFields()) {
+            throw new Error('Please fill all required fields');
+        }
+        if(this.hasPastDate()) {
+            throw new Error('Please select a future date');
+        }
+    }
+
+    hasEmptyFields() {
+        for (const field of REQUIRED_FIELDS) {
+            const value = this[field];
+
+            if (value.trim().length === 0) {
+                throw new Error(`Field "${field}" cannot be empty`);
+            }
+
+            if (Array.isArray(value) && value.length === 0) {
+                throw new Error(`Field "${field}" cannot be empty`);
+            }
+        }
+        return false;
+    }
+
+    hasPastDate() {
+        const today = new Date();
+        const bookingDate = new Date(this.startDate);
+        return bookingDate < today;
+    }
+
+    hasConflictWith(bookings: Booking[]) {
+        return bookings.some(booking => {
+            return (
+                booking.startDate === this.startDate &&
+                booking.endDate === this.endDate &&
+                booking.type === this.type &&
+                booking.roomId === this.roomId
+            );
+        });
     }
 }
---
+++ backend/domain/interfaces/BookingRepository.ts
@@ -0,0 +1,30 @@
+import {Booking, BookingData, BookingStatus} from "@domain/entities/Booking";
+
+export interface BookingRepository {
+    save(bookingData: BookingData): Promise<Booking>;
+
+    update(id: string, booking: Booking): Promise<Booking | null>;
+
+    delete(id: string): Promise<boolean>;
+
+    count(): Promise<number>;
+
+    findAll(): Promise<Booking[]>;
+
+    findById(id: string): Promise<Booking | null>;
+
+    findByType(type: string): Promise<Booking[]>;
+
+    findByStatus(status: string): Promise<Booking | null>;
+
+    findByMemberId(memberId: string): Promise<Booking[]>;
+
+    findByRoomId(roomId: string): Promise<Booking[]>;
+
+    findByStartDate(date: string): Promise<Booking[]>;
+
+    findByStartDateAndRoomId(date: string, roomId: string): Promise<Booking | null>;
+
+    findByStartDateAndMemberId(date: string, memberId: string): Promise<Booking | null>;
+}
+
